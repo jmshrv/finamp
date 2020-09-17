1c073a9cd9c36bb02ecca1e64481635c4346a269
UnicornsOnLSD <jmsharvey771@gmail.com>
2020-09-17 22:45:50 +0100

    Added view getter/saver

--- lib/services/JellyfinApiData.dart
+++ lib/services/JellyfinApiData.dart
@@ -6,6 +6,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 class JellyfinApiData {
   AuthenticationResult _currentUser;
 
+  BaseItemDto _view;
+
   /// Loads the current user from SharedPreferences if loaded for the first time or from this class if it exists. Returns null if there is no user.
   Future<AuthenticationResult> loadCurrentUser() async {
     if (_currentUser != null) {
@@ -33,5 +35,30 @@ class JellyfinApiData {
     _currentUser = newUser;
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setString("currentUser", jsonEncode(newUser));
+  }
+
+  Future<BaseItemDto> loadView() async {
+    if (_view != null) {
+      print("Getting view from memory");
+      return _view;
+    }
+
+    print("Getting view from SharedPreferences");
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String viewJson = sharedPreferences.getString("view");
+
+    if (viewJson == null) {
+      return null;
+    }
+
+    _view = BaseItemDto.fromJson(jsonDecode(viewJson));
+    return _view;
+  }
+
+  Future<void> saveView(BaseItemDto newView) async {
+    print("Saving view");
+    _view = newView;
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setString("view", jsonEncode(newView));
   }
 }
