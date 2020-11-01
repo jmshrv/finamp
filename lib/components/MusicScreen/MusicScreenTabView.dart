1261f8a8c5dd208e79f3685e6334d66b60c83244
UnicornsOnLSD <jmsharvey771@gmail.com>
2020-11-01 17:53:24 +0000

    Started adding other tabs, slightly improved error handling of getting items

--- lib/components/MusicScreen/MusicScreenTabView.dart
+++ lib/components/MusicScreen/MusicScreenTabView.dart
@@ -4,18 +4,24 @@ import 'package:get_it/get_it.dart';
 import '../../models/JellyfinModels.dart';
 import '../../services/JellyfinApiData.dart';
 import '../AlbumImage.dart';
+import '../errorSnackbar.dart';
 
-class AlbumView extends StatefulWidget {
-  const AlbumView({Key key}) : super(key: key);
+enum TabContentType { songs, albums, artists, genres, playlists }
+
+class MusicScreenTabView extends StatefulWidget {
+  const MusicScreenTabView({Key key, @required this.tabContentType})
+      : super(key: key);
+
+  final TabContentType tabContentType;
 
   @override
-  _AlbumViewState createState() => _AlbumViewState();
+  _MusicScreenTabViewState createState() => _MusicScreenTabViewState();
 }
 
 // We use AutomaticKeepAliveClientMixin so that the view keeps its position after the tab is changed.
 // https://stackoverflow.com/questions/49439047/how-to-preserve-widget-states-in-flutter-when-navigating-using-bottomnavigation
-class _AlbumViewState extends State<AlbumView>
-    with AutomaticKeepAliveClientMixin<AlbumView> {
+class _MusicScreenTabViewState extends State<MusicScreenTabView>
+    with AutomaticKeepAliveClientMixin<MusicScreenTabView> {
   @override
   bool get wantKeepAlive => true;
 
@@ -27,7 +33,11 @@ class _AlbumViewState extends State<AlbumView>
     super.initState();
     jellyfinApiData.getView().then((view) {
       albumViewFuture = jellyfinApiData.getItems(
-          parentItem: view, includeItemTypes: "MusicAlbum", sortBy: "SortName");
+          parentItem: view,
+          includeItemTypes: _includeItemTypes(widget.tabContentType),
+          sortBy: "SortName");
+
+      // We need to run setState to rebuild the widget with this new data
       setState(() {});
     });
   }
@@ -39,23 +49,47 @@ class _AlbumViewState extends State<AlbumView>
       future: albumViewFuture,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return ListView.builder(
-            itemCount: snapshot.data.length,
-            itemBuilder: (context, index) {
-              BaseItemDto album = snapshot.data[index];
+          return Scrollbar(
+            child: ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                BaseItemDto album = snapshot.data[index];
 
-              return AlbumListTile(album: album);
-            },
+                return AlbumListTile(album: album);
+              },
+            ),
           );
         } else if (snapshot.hasError) {
+          errorSnackbar(snapshot.error, context);
           return Center(
-            child: Text(snapshot.error.toString()),
+            child: Icon(Icons.error, size: 64),
           );
         } else {
           return Center(child: CircularProgressIndicator());
         }
       },
     );
+  }
+}
+
+String _includeItemTypes(TabContentType tabContentType) {
+  switch (tabContentType) {
+    case TabContentType.songs:
+      return "Audio";
+      break;
+    case TabContentType.albums:
+      return "MusicAlbum";
+      break;
+    case TabContentType.artists:
+      throw UnimplementedError("Artist view hasn't been added yet");
+      break;
+    case TabContentType.genres:
+      throw UnimplementedError("Genre view hasn't been added yet");
+    case TabContentType.playlists:
+      return "Playlist";
+      break;
+    default:
+      throw FormatException("Unsupported TabContentType");
   }
 }
 
