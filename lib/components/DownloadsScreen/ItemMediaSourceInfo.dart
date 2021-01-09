50b83021824711ca7798418c9bdc26da8ee25c39
UnicornsOnLSD <jmsharvey771@gmail.com>
2021-01-09 19:30:05 +0000

    Removed unused imports

--- lib/components/DownloadsScreen/ItemMediaSourceInfo.dart
+++ lib/components/DownloadsScreen/ItemMediaSourceInfo.dart
@@ -1,9 +1,6 @@
 import 'package:finamp/models/JellyfinModels.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:filesize/filesize.dart';
-
-import '../../services/DownloadsHelper.dart';
 
 class ItemMediaSourceInfo extends StatelessWidget {
   const ItemMediaSourceInfo({Key key, @required this.mediaSourceInfo})
