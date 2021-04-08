451e6b4770d671f67423a04c5cccf3d5b5dd88e8
UnicornsOnLSD <jmsharvey771@gmail.com>
2021-04-08 22:03:27 +0100

    Setup logging for AudioServiceHelper

--- lib/services/AudioServiceHelper.dart
+++ lib/services/AudioServiceHelper.dart
@@ -1,6 +1,7 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
+import 'package:logging/logging.dart';
 
 import 'JellyfinApiData.dart';
 import '../models/JellyfinModels.dart';
@@ -10,6 +11,7 @@ import '../services/FinampSettingsHelper.dart';
 /// Just some functions to make talking to AudioService a bit neater.
 class AudioServiceHelper {
   final _jellyfinApiData = GetIt.instance<JellyfinApiData>();
+  final audioServiceHelperLogger = Logger("AudioServiceHelper");
 
   /// Replaces the queue with the given list of items. If startAtIndex is specified, Any items below it
   /// will be ignored. This is used for when the user taps in the middle of an album to start from that point.
@@ -17,64 +19,56 @@ class AudioServiceHelper {
       {@required List<BaseItemDto> itemList,
       int startAtIndex = 0,
       bool shuffle = false}) async {
-    if (startAtIndex > itemList.length) {
-      return Future.error(
-          "startAtIndex is bigger than the itemList! ($startAtIndex > ${itemList.length})");
+    try {
+      if (startAtIndex > itemList.length) {
+        return Future.error(
+            "startAtIndex is bigger than the itemList! ($startAtIndex > ${itemList.length})");
+      }
+      List<MediaItem> queue = [];
+      for (int i = startAtIndex; i < itemList.length; i++) {
+        // Adds itemList[i] to the queue in MediaItem form.
+        // There's probably a neater way to do this for loop but it works ¯\_(ツ)_/¯
+        queue.add(MediaItem(
+          id: itemList[i].id,
+          album: itemList[i].album,
+          artist: itemList[i].albumArtist,
+          artUri: FinampSettingsHelper.finampSettings.isOffline
+              ? null
+              : "${_jellyfinApiData.currentUser.baseUrl}/Items/${itemList[i].parentId}/Images/Primary?format=jpg",
+          title: itemList[i].name,
+          extras: {"parentId": itemList[i].parentId},
+          // Jellyfin returns microseconds * 10 for some reason
+          duration: Duration(
+            microseconds: (itemList[i].runTimeTicks ~/ 10),
+          ),
+        ));
+      }
+      if (!AudioService.running) {
+        await startAudioService();
+      }
+      await AudioService.updateQueue(queue);
+      if (shuffle) {
+        await AudioService.setShuffleMode(AudioServiceShuffleMode.all);
+      }
+      _jellyfinApiData
+          .reportPlaybackStart(PlaybackProgressInfo(itemId: queue[0].id));
+      await AudioService.play();
+    } catch (e) {
+      audioServiceHelperLogger.severe(e);
+      return Future.error(e);
     }
-
-    // List<MediaItem> queue =
-    //     await Future.wait(itemList.map((BaseItemDto item) async {
-    //   if (startAtIndex >= count) {
-    //     count += 1;
-    //     return MediaItem(
-    //     id: item.id,
-    //     album: item.album,
-    //     artist: item.albumArtist,
-    //     artUri: "$baseUrl/Items/${item.id}/Images/Primary?format=webp",
-    //     title: item.name,
-    //     duration: Duration(
-    //       microseconds: (item.runTimeTicks ~/ 10),
-    //     ),
-    //   );
-    //   }
-    //   count += 1;
-    // }));
-    List<MediaItem> queue = [];
-    for (int i = startAtIndex; i < itemList.length; i++) {
-      // Adds itemList[i] to the queue in MediaItem form.
-      // There's probably a neater way to do this for loop but it works ¯\_(ツ)_/¯
-      queue.add(MediaItem(
-        id: itemList[i].id,
-        album: itemList[i].album,
-        artist: itemList[i].albumArtist,
-        artUri: FinampSettingsHelper.finampSettings.isOffline
-            ? null
-            : "${_jellyfinApiData.currentUser.baseUrl}/Items/${itemList[i].parentId}/Images/Primary?format=jpg",
-        title: itemList[i].name,
-        extras: {"parentId": itemList[i].parentId},
-        // Jellyfin returns microseconds * 10 for some reason
-        duration: Duration(
-          microseconds: (itemList[i].runTimeTicks ~/ 10),
-        ),
-      ));
-    }
-    if (!AudioService.running) {
-      await startAudioService();
-    }
-    await AudioService.updateQueue(queue);
-    if (shuffle) {
-      await AudioService.setShuffleMode(AudioServiceShuffleMode.all);
-    }
-    _jellyfinApiData
-        .reportPlaybackStart(PlaybackProgressInfo(itemId: queue[0].id));
-    await AudioService.play();
   }
 
   /// Starts the background audio service. I only wrote this function to keep
   /// _backgroundTaskEntrypoint in one place.
   Future<void> startAudioService() async {
-    await AudioService.start(
-        backgroundTaskEntrypoint: _backgroundTaskEntrypoint);
+    try {
+      await AudioService.start(
+          backgroundTaskEntrypoint: _backgroundTaskEntrypoint);
+    } catch (e) {
+      audioServiceHelperLogger.severe(e);
+      return Future.error(e);
+    }
   }
 }
 
