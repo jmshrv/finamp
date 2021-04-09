9b852e43411bc22f55e6d6b8327b09e940d83d6f
UnicornsOnLSD <jmsharvey771@gmail.com>
2021-04-09 16:56:25 +0100

    Make LogsScreen actually use _tabController (fixes logs not copying)

--- lib/screens/LogsScreen.dart
+++ lib/screens/LogsScreen.dart
@@ -24,8 +24,10 @@ class _LogsScreenState extends State<LogsScreen>
   @override
   void initState() {
     super.initState();
-    _tabController =
-        new TabController(vsync: this, length: LogsScreen.tabs.length);
+    _tabController = new TabController(
+      vsync: this,
+      length: LogsScreen.tabs.length,
+    );
   }
 
   @override
@@ -38,40 +40,41 @@ class _LogsScreenState extends State<LogsScreen>
   Widget build(BuildContext context) {
     FinampLogsHelper finampLogsHelper = GetIt.instance<FinampLogsHelper>();
 
-    return DefaultTabController(
-      length: LogsScreen.tabs.length,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Logs"),
-          bottom: TabBar(tabs: LogsScreen.tabs),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.copy),
-              onPressed: () async {
-                if (_tabController.index == 1) {
-                  await finampLogsHelper.copyLogs();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Logs"),
+        bottom: TabBar(
+          tabs: LogsScreen.tabs,
+          controller: _tabController,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.copy),
+            onPressed: () async {
+              if (_tabController.index == 0) {
+                await finampLogsHelper.copyLogs();
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Main thread logs copied")));
+              } else {
+                if (!AudioService.running) {
                   ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("Main thread logs copied")));
+                      SnackBar(content: Text("Audio service is not running")));
                 } else {
-                  if (!AudioService.running) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text("Audio service is not running")));
-                  } else {
-                    await AudioService.customAction("copyLogs");
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text("Audio service logs copied")));
-                  }
+                  await AudioService.customAction("copyLogs");
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Audio service logs copied")));
                 }
-              },
-            )
-          ],
-        ),
-        body: TabBarView(
-          children: [
-            LogsView(isMusicPlayerBackgroundTask: false),
-            LogsView(isMusicPlayerBackgroundTask: true),
-          ],
-        ),
+              }
+            },
+          )
+        ],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          LogsView(isMusicPlayerBackgroundTask: false),
+          LogsView(isMusicPlayerBackgroundTask: true),
+        ],
       ),
     );
   }
