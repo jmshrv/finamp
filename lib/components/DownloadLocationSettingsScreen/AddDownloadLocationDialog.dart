2c826cb260a2997a726f6ca20d786924865a2726
UnicornsOnLSD <jmsharvey771@gmail.com>
2021-04-16 18:49:12 +0100

    Started work on add download location dialog

---
+++ lib/components/DownloadLocationSettingsScreen/AddDownloadLocationDialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddDownloadLocationDialog extends StatelessWidget {
+  const AddDownloadLocationDialog({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog();
+  }
+}
