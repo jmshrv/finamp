a919a134cdffc52d2f083a9e00e369b22b1fef4d
UnicornsOnLSD <44349936+UnicornsOnLSD@users.noreply.github.com>
2021-04-29 19:10:40 +0100

    Add scrollbar to DownloadsScreen

--- lib/screens/DownloadsScreen.dart
+++ lib/screens/DownloadsScreen.dart
@@ -14,21 +14,23 @@ class DownloadsScreen extends StatelessWidget {
         title: Text("Downloads"),
         actions: [DownloadErrorScreenButton()],
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate([
-              Padding(
-                // We don't have bottom padding here since the divider already provides bottom padding
-                padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-                child: DownloadsOverview(),
-              ),
-              Divider(),
-            ]),
-          ),
-          DownloadedAlbumsList(),
-          // CurrentDownloadsList(),
-        ],
+      body: Scrollbar(
+        child: CustomScrollView(
+          slivers: [
+            SliverList(
+              delegate: SliverChildListDelegate([
+                Padding(
+                  // We don't have bottom padding here since the divider already provides bottom padding
+                  padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                  child: DownloadsOverview(),
+                ),
+                Divider(),
+              ]),
+            ),
+            DownloadedAlbumsList(),
+            // CurrentDownloadsList(),
+          ],
+        ),
       ),
     );
   }
