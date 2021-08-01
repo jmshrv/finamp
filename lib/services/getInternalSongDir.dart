5f9f9bf81d7648a85e839f99d4850b09b0fe3380
UnicornsOnLSD <44349936+UnicornsOnLSD@users.noreply.github.com>
2021-08-01 02:45:41 +0100

    Hide documents dir in iOS

--- lib/services/getInternalSongDir.dart
+++ lib/services/getInternalSongDir.dart
@@ -5,6 +5,7 @@ import 'package:path_provider/path_provider.dart';
 /// Returns the "internal storage" directory for songs (applicationDocumentsDirectory + /songs).
 /// If it doesn't exist, the directory is created.
 Future<Directory> getInternalSongDir() async {
+  // TODO: Start using support directory by default, keep this around for legacy
   Directory appDir = await getApplicationDocumentsDirectory();
   Directory songDir = Directory(appDir.path + "/songs");
   if (!await songDir.exists()) {
