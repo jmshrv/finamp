1dcdc488210f678cd78b7a98239c663dd607f8e1
rom4nik <46846000+rom4nik@users.noreply.github.com>
2021-09-14 20:26:08 +0200

    Remove unnecessary imports

--- lib/components/AlbumScreen/AlbumScreenContent.dart
+++ lib/components/AlbumScreen/AlbumScreenContent.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
 import '../../models/JellyfinModels.dart';
 import '../../services/FinampSettingsHelper.dart';
