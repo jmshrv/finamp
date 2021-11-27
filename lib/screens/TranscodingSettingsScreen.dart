abc9299114bdb2c66309132374e524ec744a0c07
UnicornsOnLSD <44349936+UnicornsOnLSD@users.noreply.github.com>
2021-11-27 19:53:27 +0000

    Hide Vorbis option on iOS and macOS

--- lib/screens/TranscodingSettingsScreen.dart
+++ lib/screens/TranscodingSettingsScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import '../components/TranscodingSettingsScreen/TranscodeSwitch.dart';
@@ -18,7 +20,7 @@ class TranscodingSettingsScreen extends StatelessWidget {
           children: [
             const TranscodeSwitch(),
             const BitrateSelector(),
-            const VorbisSwitch(),
+            if (!(Platform.isIOS || Platform.isMacOS)) const VorbisSwitch(),
           ],
         ),
       ),
