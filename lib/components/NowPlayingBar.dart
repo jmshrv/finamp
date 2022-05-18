4e430d4f83e0de63894eeb9953e60608daff128c
rom4nik <46846000+rom4nik@users.noreply.github.com>
2022-05-18 20:33:15 +0200

    Add playback progress bar on NowPlayingBar (#198)
    * Add playback progress bar on NowPlayingBar
    
    * Reuse ProgressSlider for NowPlayingBar, change slider shape to rectangular if not seekable
    
    * Stack ProgressSlider and Dismissible in NowPlayingBar

--- lib/components/NowPlayingBar.dart
+++ lib/components/NowPlayingBar.dart
@@ -4,10 +4,11 @@ import 'package:get_it/get_it.dart';
 
 import '../components/AlbumImage.dart';
 import '../models/JellyfinModels.dart';
-import '../services/mediaStateStream.dart';
+import '../services/progressStateStream.dart';
 import '../services/FinampSettingsHelper.dart';
 import '../services/processArtist.dart';
 import '../services/MusicPlayerBackgroundTask.dart';
+import 'PlayerScreen/ProgressSlider.dart';
 
 class NowPlayingBar extends StatelessWidget {
   const NowPlayingBar({
@@ -22,8 +23,8 @@ class NowPlayingBar extends StatelessWidget {
 
     final audioHandler = GetIt.instance<MusicPlayerBackgroundTask>();
 
-    return StreamBuilder<MediaState>(
-      stream: mediaStateStream,
+    return StreamBuilder<ProgressState>(
+      stream: progressStateStream,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final playing = snapshot.data!.playbackState.playing;
@@ -37,91 +38,101 @@ class NowPlayingBar extends StatelessWidget {
               child: SafeArea(
                 child: SizedBox(
                   width: MediaQuery.of(context).size.width,
-                  child: Dismissible(
-                    key: const Key("NowPlayingBar"),
-                    confirmDismiss: (direction) async {
-                      if (direction == DismissDirection.endToStart) {
-                        audioHandler.skipToNext();
-                      } else {
-                        audioHandler.skipToPrevious();
-                      }
-                      return false;
-                    },
-                    background: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          AspectRatio(
-                            aspectRatio: 1,
-                            child: FittedBox(
-                              fit: BoxFit.fitHeight,
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(vertical: 8.0),
-                                child: Icon(Icons.skip_previous),
-                              ),
-                            ),
-                          ),
-                          AspectRatio(
-                            aspectRatio: 1,
-                            child: FittedBox(
-                              fit: BoxFit.fitHeight,
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(vertical: 8.0),
-                                child: Icon(Icons.skip_next),
-                              ),
-                            ),
-                          ),
-                        ],
+                  child: Stack(
+                    children: [
+                      const ProgressSlider(
+                        allowSeeking: false,
+                        showBuffer: false,
+                        showDuration: false,
+                        showPlaceholder: false,
                       ),
-                    ),
-                    child: ListTile(
-                      onTap: () =>
-                          Navigator.of(context).pushNamed("/nowplaying"),
-                      // We put the album image in a ValueListenableBuilder so that it reacts to offline changes
-                      leading: ValueListenableBuilder(
-                        valueListenable:
-                            FinampSettingsHelper.finampSettingsListener,
-                        builder: (context, _, widget) => AlbumImage(
-                          item: BaseItemDto.fromJson(
-                              snapshot.data!.mediaItem!.extras!["itemJson"]),
+                      Dismissible(
+                        key: const Key("NowPlayingBar"),
+                        confirmDismiss: (direction) async {
+                          if (direction == DismissDirection.endToStart) {
+                            audioHandler.skipToNext();
+                          } else {
+                            audioHandler.skipToPrevious();
+                          }
+                          return false;
+                        },
+                        background: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              AspectRatio(
+                                aspectRatio: 1,
+                                child: FittedBox(
+                                  fit: BoxFit.fitHeight,
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                                    child: Icon(Icons.skip_previous),
+                                  ),
+                                ),
+                              ),
+                              AspectRatio(
+                                aspectRatio: 1,
+                                child: FittedBox(
+                                  fit: BoxFit.fitHeight,
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 8.0),
+                                    child: Icon(Icons.skip_next),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        child: ListTile(
+                          onTap: () =>
+                              Navigator.of(context).pushNamed("/nowplaying"),
+                          // We put the album image in a ValueListenableBuilder so that it reacts to offline changes
+                          leading: ValueListenableBuilder(
+                            valueListenable:
+                                FinampSettingsHelper.finampSettingsListener,
+                            builder: (context, _, widget) => AlbumImage(
+                              item: BaseItemDto.fromJson(
+                                  snapshot.data!.mediaItem!.extras!["itemJson"]),
+                            ),
+                          ),
+                          title: Text(
+                            snapshot.data!.mediaItem!.title,
+                            softWrap: false,
+                            maxLines: 1,
+                            overflow: TextOverflow.fade,
+                          ),
+                          subtitle: Text(
+                            processArtist(snapshot.data!.mediaItem!.artist),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              if (snapshot.data!.playbackState.processingState !=
+                                  AudioProcessingState.idle)
+                                IconButton(
+                                  // We have a key here because otherwise the
+                                  // InkWell moves over to the play/pause button
+                                  key: const ValueKey("StopButton"),
+                                  icon: const Icon(Icons.stop),
+                                  onPressed: () => audioHandler.stop(),
+                                ),
+                              playing
+                                  ? IconButton(
+                                      icon: const Icon(Icons.pause),
+                                      onPressed: () => audioHandler.pause(),
+                                    )
+                                  : IconButton(
+                                      icon: const Icon(Icons.play_arrow),
+                                      onPressed: () => audioHandler.play(),
+                                    ),
+                            ],
+                          ),
                         ),
                       ),
-                      title: Text(
-                        snapshot.data!.mediaItem!.title,
-                        softWrap: false,
-                        maxLines: 1,
-                        overflow: TextOverflow.fade,
-                      ),
-                      subtitle: Text(
-                        processArtist(snapshot.data!.mediaItem!.artist),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          if (snapshot.data!.playbackState.processingState !=
-                              AudioProcessingState.idle)
-                            IconButton(
-                              // We have a key here because otherwise the
-                              // InkWell moves over to the play/pause button
-                              key: const ValueKey("StopButton"),
-                              icon: const Icon(Icons.stop),
-                              onPressed: () => audioHandler.stop(),
-                            ),
-                          playing
-                              ? IconButton(
-                                  icon: const Icon(Icons.pause),
-                                  onPressed: () => audioHandler.pause(),
-                                )
-                              : IconButton(
-                                  icon: const Icon(Icons.play_arrow),
-                                  onPressed: () => audioHandler.play(),
-                                ),
-                        ],
-                      ),
-                    ),
+                    ],
                   ),
                 ),
               ),
