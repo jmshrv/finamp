7cd87c3bac76f48c49600d673913e1ece0473d69
James Harvey <44349936+jmshrv@users.noreply.github.com>
2022-08-17 23:16:14 +0100

    Localise error snackbar

--- lib/components/error_snackbar.dart
+++ lib/components/error_snackbar.dart
@@ -1,23 +1,24 @@
 import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 /// Snackbar with error icon for displaying errors
 ScaffoldFeatureController<SnackBar, SnackBarClosedReason> errorSnackbar(
     dynamic error, BuildContext context) {
   return ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      content: const Text("An error has occurred."),
+      content: Text(AppLocalizations.of(context)!.anErrorHasOccured),
       action: SnackBarAction(
-        label: "MORE",
+        label: MaterialLocalizations.of(context).moreButtonTooltip,
         onPressed: () => showDialog(
           context: context,
           builder: (context) => AlertDialog(
-            title: const Text("Error"),
-            content: Text(_errorText(error)),
+            title: Text(AppLocalizations.of(context)!.error),
+            content: Text(_errorText(error, context)),
             actions: [
               TextButton(
                 onPressed: () => Navigator.of(context).pop(),
-                child: const Text("CLOSE"),
+                child: Text(MaterialLocalizations.of(context).closeButtonLabel),
               )
             ],
           ),
@@ -27,12 +28,16 @@ ScaffoldFeatureController<SnackBar, SnackBarClosedReason> errorSnackbar(
   );
 }
 
-String _errorText(dynamic error) {
+String _errorText(dynamic error, BuildContext context) {
   if (error.runtimeType == Response) {
+    error = error as Response;
+
     if (error.statusCode == 401) {
-      return "${error.error.toString()} Status code ${error.statusCode}. This probably means that you used the wrong username/password, or your client is no longer authenticated.";
+      return AppLocalizations.of(context)!
+          .responseError401(error.error.toString(), error.statusCode);
     } else {
-      return "${error.error.toString()} Status code ${error.statusCode}.";
+      return AppLocalizations.of(context)!
+          .responseError(error.error.toString(), error.statusCode);
     }
   } else {
     return error.toString();
