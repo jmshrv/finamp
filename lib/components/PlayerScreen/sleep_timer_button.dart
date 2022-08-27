bdce269c0451be2800cc5d5cba70e4bd9f97bae4
James Harvey <44349936+jmshrv@users.noreply.github.com>
2022-08-27 12:32:47 +0100

    Fix sleep timer button orientation in RTL

--- lib/components/PlayerScreen/sleep_timer_button.dart
+++ lib/components/PlayerScreen/sleep_timer_button.dart
@@ -8,6 +8,23 @@ import '../../services/music_player_background_task.dart';
 import 'sleep_timer_dialog.dart';
 import 'sleep_timer_cancel_dialog.dart';
 
+// Flutter doesn't flip this icon in RTL layouts, which apparently isn't correct
+// behaviour. We fix this by defining IconDatas with matchTextDirection set to
+// true. See #310 for more info.
+final directionalModeNight = IconData(
+  Icons.mode_night.codePoint,
+  fontFamily: Icons.mode_night.fontFamily,
+  fontPackage: Icons.mode_night.fontPackage,
+  matchTextDirection: true,
+);
+
+final directionalModeNightOutlined = IconData(
+  Icons.mode_night_outlined.codePoint,
+  fontFamily: Icons.mode_night_outlined.fontFamily,
+  fontPackage: Icons.mode_night_outlined.fontPackage,
+  matchTextDirection: true,
+);
+
 class SleepTimerButton extends StatelessWidget {
   const SleepTimerButton({Key? key}) : super(key: key);
 
@@ -20,8 +37,8 @@ class SleepTimerButton extends StatelessWidget {
       builder: (context, value, child) {
         return IconButton(
             icon: value == null
-                ? const Icon(Icons.mode_night_outlined)
-                : const Icon(Icons.mode_night),
+                ? Icon(directionalModeNightOutlined)
+                : Icon(directionalModeNight),
             tooltip: AppLocalizations.of(context)!.sleepTimerTooltip,
             onPressed: () async {
               if (value != null) {
