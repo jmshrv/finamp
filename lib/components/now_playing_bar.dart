4a1165cd439e79d627a3789abb455c3c92e9d024
Yonggan <yonggan@obco.pro>
2022-10-06 17:02:57 +0200

    Add Gesture to swipe up to open the player

--- lib/components/now_playing_bar.dart
+++ lib/components/now_playing_bar.dart
@@ -1,6 +1,7 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:simple_gesture_detector/simple_gesture_detector.dart';
 
 import '../services/media_state_stream.dart';
 import 'album_image.dart';
@@ -23,132 +24,140 @@ class NowPlayingBar extends StatelessWidget {
 
     final audioHandler = GetIt.instance<MusicPlayerBackgroundTask>();
 
-    return StreamBuilder<MediaState>(
-      stream: mediaStateStream,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final playing = snapshot.data!.playbackState.playing;
+    return SimpleGestureDetector(
+      onVerticalSwipe: (direction) {
+        if (direction == SwipeDirection.up) {
+          Navigator.of(context)
+              .pushNamed(PlayerScreen.routeName);
+        }
+      },
+      child: StreamBuilder<MediaState>(
+        stream: mediaStateStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final playing = snapshot.data!.playbackState.playing;
 
-          // If we have a media item and the player hasn't finished, show
-          // the now playing bar.
-          if (snapshot.data!.mediaItem != null) {
-            final item = BaseItemDto.fromJson(
-                snapshot.data!.mediaItem!.extras!["itemJson"]);
+            // If we have a media item and the player hasn't finished, show
+            // the now playing bar.
+            if (snapshot.data!.mediaItem != null) {
+              final item = BaseItemDto.fromJson(
+                  snapshot.data!.mediaItem!.extras!["itemJson"]);
 
-            return Material(
-              color: color,
-              elevation: elevation,
-              child: SafeArea(
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Stack(
-                    children: [
-                      const ProgressSlider(
-                        allowSeeking: false,
-                        showBuffer: false,
-                        showDuration: false,
-                        showPlaceholder: false,
-                      ),
-                      Dismissible(
-                        key: const Key("NowPlayingBar"),
-                        confirmDismiss: (direction) async {
-                          if (direction == DismissDirection.endToStart) {
-                            audioHandler.skipToNext();
-                          } else {
-                            audioHandler.skipToPrevious();
-                          }
-                          return false;
-                        },
-                        background: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: const [
-                              AspectRatio(
-                                aspectRatio: 1,
-                                child: FittedBox(
-                                  fit: BoxFit.fitHeight,
-                                  child: Padding(
-                                    padding:
-                                        EdgeInsets.symmetric(vertical: 8.0),
-                                    child: Icon(Icons.skip_previous),
-                                  ),
-                                ),
-                              ),
-                              AspectRatio(
-                                aspectRatio: 1,
-                                child: FittedBox(
-                                  fit: BoxFit.fitHeight,
-                                  child: Padding(
-                                    padding:
-                                        EdgeInsets.symmetric(vertical: 8.0),
-                                    child: Icon(Icons.skip_next),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+              return Material(
+                color: color,
+                elevation: elevation,
+                child: SafeArea(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Stack(
+                      children: [
+                        const ProgressSlider(
+                          allowSeeking: false,
+                          showBuffer: false,
+                          showDuration: false,
+                          showPlaceholder: false,
                         ),
-                        child: ListTile(
-                          onTap: () => Navigator.of(context)
-                              .pushNamed(PlayerScreen.routeName),
-                          leading: AlbumImage(item: item),
-                          title: Text(
-                            snapshot.data!.mediaItem!.title,
-                            softWrap: false,
-                            maxLines: 1,
-                            overflow: TextOverflow.fade,
-                          ),
-                          subtitle: Text(
-                            processArtist(
-                                snapshot.data!.mediaItem!.artist, context),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              if (snapshot
-                                      .data!.playbackState.processingState !=
-                                  AudioProcessingState.idle)
-                                IconButton(
-                                  // We have a key here because otherwise the
-                                  // InkWell moves over to the play/pause button
-                                  key: const ValueKey("StopButton"),
-                                  icon: const Icon(Icons.stop),
-                                  onPressed: () => audioHandler.stop(),
-                                ),
-                              playing
-                                  ? IconButton(
-                                      icon: const Icon(Icons.pause),
-                                      onPressed: () => audioHandler.pause(),
-                                    )
-                                  : IconButton(
-                                      icon: const Icon(Icons.play_arrow),
-                                      onPressed: () => audioHandler.play(),
+                        Dismissible(
+                          key: const Key("NowPlayingBar"),
+                          confirmDismiss: (direction) async {
+                            if (direction == DismissDirection.endToStart) {
+                              audioHandler.skipToNext();
+                            } else {
+                              audioHandler.skipToPrevious();
+                            }
+                            return false;
+                          },
+                          background: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: const [
+                                AspectRatio(
+                                  aspectRatio: 1,
+                                  child: FittedBox(
+                                    fit: BoxFit.fitHeight,
+                                    child: Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 8.0),
+                                      child: Icon(Icons.skip_previous),
                                     ),
-                            ],
+                                  ),
+                                ),
+                                AspectRatio(
+                                  aspectRatio: 1,
+                                  child: FittedBox(
+                                    fit: BoxFit.fitHeight,
+                                    child: Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 8.0),
+                                      child: Icon(Icons.skip_next),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          child: ListTile(
+                            onTap: () => Navigator.of(context)
+                                .pushNamed(PlayerScreen.routeName),
+                            leading: AlbumImage(item: item),
+                            title: Text(
+                              snapshot.data!.mediaItem!.title,
+                              softWrap: false,
+                              maxLines: 1,
+                              overflow: TextOverflow.fade,
+                            ),
+                            subtitle: Text(
+                              processArtist(
+                                  snapshot.data!.mediaItem!.artist, context),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                if (snapshot
+                                        .data!.playbackState.processingState !=
+                                    AudioProcessingState.idle)
+                                  IconButton(
+                                    // We have a key here because otherwise the
+                                    // InkWell moves over to the play/pause button
+                                    key: const ValueKey("StopButton"),
+                                    icon: const Icon(Icons.stop),
+                                    onPressed: () => audioHandler.stop(),
+                                  ),
+                                playing
+                                    ? IconButton(
+                                        icon: const Icon(Icons.pause),
+                                        onPressed: () => audioHandler.pause(),
+                                      )
+                                    : IconButton(
+                                        icon: const Icon(Icons.play_arrow),
+                                        onPressed: () => audioHandler.play(),
+                                      ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            );
+              );
+            } else {
+              return const SizedBox(
+                width: 0,
+                height: 0,
+              );
+            }
           } else {
             return const SizedBox(
               width: 0,
               height: 0,
             );
           }
-        } else {
-          return const SizedBox(
-            width: 0,
-            height: 0,
-          );
-        }
-      },
+        },
+      ),
     );
   }
 }
