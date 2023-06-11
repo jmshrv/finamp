4d08fb064c01c83be5b6c6ed6376a4695b09cf62
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-06-11 21:09:02 +0100

    Censor login information in logs screen

---
+++ lib/services/censored_log.dart
@@ -0,0 +1,33 @@
+import 'package:finamp/services/contains_login.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logging/logging.dart';
+
+import 'case_insensitive_pattern.dart';
+import 'finamp_user_helper.dart';
+
+extension CensoredMessage on LogRecord {
+  /// The uncensored log string to be shown to the user
+  String get logString =>
+      "[$loggerName/${level.name}] $time: $message\n\n${stackTrace.toString()}";
+
+  String get loginCensoredMessage => containsLogin ? "LOGIN BODY" : message;
+
+  String get censoredMessage {
+    if (containsLogin) {
+      return loginCensoredMessage;
+    }
+
+    final finampUserHelper = GetIt.instance<FinampUserHelper>();
+
+    String workingLogString = logString;
+
+    for (final user in finampUserHelper.finampUsers) {
+      workingLogString =
+          logString.replaceAll(CaseInsensitivePattern(user.baseUrl), "BASEURL");
+      workingLogString = logString.replaceAll(
+          CaseInsensitivePattern(user.accessToken), "TOKEN");
+    }
+
+    return workingLogString;
+  }
+}
