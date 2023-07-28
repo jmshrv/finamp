da5f4b22dffda58caf3b9079c1150530d41ba0db
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-07-28 11:56:24 +0100

    Merge requiredBy when migrating blurhashes

--- lib/services/downloads_helper.dart
+++ lib/services/downloads_helper.dart
@@ -884,6 +884,26 @@ class DownloadsHelper {
         .where((element) => !imagesToKeep.contains(element))
         .toList();
 
+    for (final image in imagesToDelete) {
+      final song = getDownloadedSong(image.requiredBy.first);
+
+      if (song != null) {
+        final blurHash = song.song.blurHash;
+
+        imageMap[blurHash]?.requiredBy.addAll(image.requiredBy);
+      }
+    }
+
+    // Go through each requiredBy and remove duplicates
+    for (final imageEntry in imageMap.entries) {
+      final image = imageEntry.value;
+
+      image.requiredBy = image.requiredBy.toSet().toList();
+      _downloadsLogger.warning(image.requiredBy);
+
+      imageMap[imageEntry.key] = image;
+    }
+
     // Sanity check to make sure we haven't double counted/missed an image.
     final imagesCount = imagesToKeep.length + imagesToDelete.length;
     if (imagesCount != downloadedImages.length) {
