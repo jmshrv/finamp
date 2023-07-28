db9bbcf9825c80ed47c80a5c4cbd9f94ed7561c5
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-07-28 11:01:39 +0100

    Run dart fix

--- lib/screens/tabs_settings_screen.dart
+++ lib/screens/tabs_settings_screen.dart
@@ -2,7 +2,6 @@ import 'package:finamp/services/finamp_settings_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import '../models/finamp_models.dart';
 import '../components/TabsSettingsScreen/hide_tab_toggle.dart';
 
 class TabsSettingsScreen extends StatefulWidget {
