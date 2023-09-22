d56f909493229d2c23c3cc24b5434a84d6eaefe3
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-09-22 12:52:42 +0100

    Make view selector only allow music views
    * Disables non-music list options
    * If there are no music views, show an error message

--- lib/screens/view_selector.dart
+++ lib/screens/view_selector.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get_it/get_it.dart';
 
+import '../components/ViewSelector/no_music_libraries_message.dart';
 import '../services/finamp_user_helper.dart';
 import 'music_screen.dart';
 import '../services/jellyfin_api_helper.dart';
@@ -22,6 +23,7 @@ class _ViewSelectorState extends State<ViewSelector> {
   final _finampUserHelper = GetIt.instance<FinampUserHelper>();
   late Future<List<BaseItemDto>> viewListFuture;
   final Map<BaseItemDto, bool> _views = {};
+  bool isSubmitButtonEnabled = false;
 
   @override
   void initState() {
@@ -35,29 +37,36 @@ class _ViewSelectorState extends State<ViewSelector> {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.selectMusicLibraries),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _submitChoice,
-        child: const Icon(Icons.check),
-      ),
+      floatingActionButton: isSubmitButtonEnabled
+          ? FloatingActionButton(
+              onPressed: _submitChoice,
+              child: const Icon(Icons.check),
+            )
+          : null,
       body: FutureBuilder<List<BaseItemDto>>(
         future: viewListFuture,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            if (snapshot.data!.isEmpty) {
-              // If snapshot.data is empty, getMusicViews returned no music libraries. This means that the user doesn't have any music libraries.
-              return Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child:
-                      Text(AppLocalizations.of(context)!.couldNotFindLibraries),
-                ),
+            // Finamp only supports music libraries. We used to allow people to
+            // select unsupported libraries, but some people selected "general"
+            // libraries and thought Finamp was broken.
+            if (snapshot.data!.isEmpty ||
+                !snapshot.data!
+                    .any((element) => element.collectionType == "music")) {
+              return NoMusicLibrariesMessage(
+                onRefresh: () {
+                  setState(() {
+                    _views.clear();
+                    viewListFuture = _jellyfinApiHelper.getViews();
+                  });
+                },
               );
-            } else {
-              if (_views.isEmpty) {
-                _views.addEntries(snapshot.data!
-                    .where((element) => element.collectionType != "playlists")
-                    .map((e) => MapEntry(e, e.collectionType == "music")));
-              }
+            }
+
+            if (_views.isEmpty) {
+              _views.addEntries(snapshot.data!
+                  .where((element) => element.collectionType != "playlists")
+                  .map((e) => MapEntry(e, e.collectionType == "music")));
 
               // If only one music library is available and user doesn't have a
               // view saved (assuming setup is in progress), skip the selector.
@@ -66,37 +75,30 @@ class _ViewSelectorState extends State<ViewSelector> {
                   _finampUserHelper.currentUser!.currentView == null) {
                 _submitChoice();
               }
-
-              return Scrollbar(
-                child: ListView.builder(
-                  itemCount: _views.length,
-                  itemBuilder: (context, index) {
-                    return CheckboxListTile(
-                      value: _views.values.elementAt(index),
-                      title: Text(_views.keys.elementAt(index).name ??
-                          AppLocalizations.of(context)!.unknownName),
-                      onChanged: (value) {
-                        setState(() {
-                          _views[_views.keys.elementAt(index)] = value!;
-                        });
-                      },
-                      // onTap: () async {
-                      //   JellyfinApiData jellyfinApiHelper =
-                      //       GetIt.instance<JellyfinApiData>();
-                      //   try {
-                      //     jellyfinApiHelper.saveView(snapshot.data![index],
-                      //         jellyfinApiHelper.currentUser!.id);
-                      //     Navigator.of(context)
-                      //         .pushNamedAndRemoveUntil("/music", (route) => false);
-                      //   } catch (e) {
-                      //     errorSnackbar(e, context);
-                      //   }
-                      // },
-                    );
-                  },
-                ),
-              );
             }
+
+            return Scrollbar(
+              child: ListView.builder(
+                itemCount: _views.length,
+                itemBuilder: (context, index) {
+                  final isSelected = _views.values.elementAt(index);
+                  final view = _views.keys.elementAt(index);
+
+                  return CheckboxListTile(
+                    value: isSelected,
+                    enabled: view.collectionType == "music",
+                    title: Text(_views.keys.elementAt(index).name ??
+                        AppLocalizations.of(context)!.unknownName),
+                    onChanged: (value) {
+                      setState(() {
+                        _views[_views.keys.elementAt(index)] = value!;
+                        isSubmitButtonEnabled = _views.values.contains(true);
+                      });
+                    },
+                  );
+                },
+              ),
+            );
           } else if (snapshot.hasError) {
             errorSnackbar(snapshot.error, context);
             // TODO: Let the user refresh the page
