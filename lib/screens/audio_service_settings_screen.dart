41d3c02c40d218b2c8cc617c8536141533559769
Gabriel Jann <mail@jannga.org>
2024-02-03 17:03:54 +0100

    Add 'swipeInsertQueueNext' to settings menu

--- lib/screens/audio_service_settings_screen.dart
+++ lib/screens/audio_service_settings_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../components/AudioServiceSettingsScreen/buffer_duration_list_tile.dart';
 import '../components/AudioServiceSettingsScreen/stop_foreground_selector.dart';
 import '../components/AudioServiceSettingsScreen/song_shuffle_item_count_editor.dart';
+import '../components/AudioServiceSettingsScreen/swipe_insert_queue_next_selector.dart';
 
 class AudioServiceSettingsScreen extends StatelessWidget {
   const AudioServiceSettingsScreen({Key? key}) : super(key: key);
@@ -22,6 +23,7 @@ class AudioServiceSettingsScreen extends StatelessWidget {
         child: ListView(
           children: [
             if (Platform.isAndroid) const StopForegroundSelector(),
+            const SwipeInsertQueueNextSelector(),
             const SongShuffleItemCountEditor(),
             const BufferDurationListTile(),
           ],
