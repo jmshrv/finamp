7b5f2954fbb9358f51a361cdc561a25b490dd269
Chaphasilor <ppp.u@web.de>
2024-02-10 00:22:07 +0100

    adapt airplay tint color and try to prevent overflow

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
+import 'package:finamp/color_schemes.g.dart';
 import 'package:finamp/components/PlayerScreen/player_screen_appbar_title.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
 import 'package:simple_gesture_detector/simple_gesture_detector.dart';
 import 'package:flutter_to_airplay/flutter_to_airplay.dart';
 
@@ -72,7 +75,16 @@ class _PlayerScreenContent extends StatelessWidget {
             onPressed: () => Navigator.of(context).pop(),
           ),
           actions: [
-            if (Platform.isIOS) AirPlayRoutePickerView(),
+            if (Platform.isIOS)
+              SizedBox(
+                width: 44.0,
+                height: 44.0,
+                child: AirPlayRoutePickerView(
+                  tintColor: IconTheme.of(context).color ?? Colors.white,
+                  activeTintColor: jellyfinBlueColor,
+                  onShowPickerView: () => Vibrate.feedback(FeedbackType.selection),
+                ),
+              ),
           ],
         ),
         // Required for sleep timer input
