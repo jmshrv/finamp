1cfc5558ca0da3907725f41ec76a0ef3d18a69c1
lymnyx <lymnyx@protonmail.com>
2024-02-20 01:38:39 +0100

    Scroll to active preset

--- lib/components/AlbumScreen/preset_chip.dart
+++ lib/components/AlbumScreen/preset_chip.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:finamp/services/queue_service.dart';
 import 'package:get_it/get_it.dart';
@@ -28,43 +29,79 @@ class PresetChips extends StatefulWidget {
   final Color? mainColour; // used for different background colours
   final Function()? onPressed;
 
+  final chipWidth = 55.0;
+
   @override
   State<PresetChips> createState() => _PresetChipsState();
 }
 
 class _PresetChipsState extends State<PresetChips> {
+  final _controller = ScrollController();
+  var scrolledAlready = false;
+
+  scrollToActivePreset(double currentValue, double maxWidth) {
+    if (!_controller.hasClients) return false;
+    var offset =
+        (widget.chipWidth + 8.0) * widget.values.indexOf(currentValue) -
+            maxWidth / 2 +
+            widget.chipWidth / 2;
+
+    offset = min(
+        max(0, offset), maxWidth - _controller.position.maxScrollExtent + 15);
+
+    _controller.animateTo(
+      offset,
+      duration: Duration(milliseconds: 350),
+      curve: Curves.easeOutCubic,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var nowActiveValue = widget.activeValue;
 
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Wrap(
-        spacing: 8.0,
-        runSpacing: 8.0,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        children: List.generate(widget.values.length, (index) {
-          final currentValue = widget.values[index];
-          var newValue = "x$currentValue";
-    
-          return PresetChip(
-            value: newValue,
-            backgroundColour: currentValue == nowActiveValue
-                ? widget.mainColour?.withOpacity(0.4)
-                : widget.mainColour?.withOpacity(0.1),
-            isTextBold: currentValue == 1.0,
-            width: 55.0,
-            onTap: () {
-              setState(() {
-                nowActiveValue = currentValue;
-              });
-              _queueService.setPlaybackSpeed(currentValue);
-              widget.onPressed!();
-            },
-          );
-        }),
-      ),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      return SingleChildScrollView(
+        controller: _controller,
+        scrollDirection: Axis.horizontal,
+        child: Wrap(
+          spacing: 8.0,
+          runSpacing: 8.0,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: List.generate(widget.values.length, (index) {
+            final currentValue = widget.values[index];
+            var newValue = "x$currentValue";
+
+            if (currentValue == nowActiveValue) {
+              if (scrolledAlready) {
+                scrollToActivePreset(currentValue, constraints.maxWidth);
+              } else {
+                Future.delayed(Duration(milliseconds: 200), () {
+                  scrollToActivePreset(currentValue, constraints.maxWidth);
+                });
+                scrolledAlready = true;
+              }
+            }
+
+            return PresetChip(
+              value: newValue,
+              backgroundColour: currentValue == nowActiveValue
+                  ? widget.mainColour?.withOpacity(0.4)
+                  : widget.mainColour?.withOpacity(0.1),
+              isTextBold: currentValue == 1.0,
+              width: widget.chipWidth,
+              onTap: () {
+                setState(() {
+                  nowActiveValue = currentValue;
+                });
+                _queueService.setPlaybackSpeed(currentValue);
+                widget.onPressed!();
+              },
+            );
+          }),
+        ),
+      );
+    });
   }
 }
 
