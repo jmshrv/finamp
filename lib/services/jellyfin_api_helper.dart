jmshrv/finamp

commit 9b983864b9f1c1a899fc4d7c6c3f6b351b862683

Various tweaks to hopefully improve performance and clean up some possible stuck states.  Switched to using MediaSources in BaseItemDto instead of seperate, reduced dedicated getPlaybackInfo calls.

lib/services/jellyfin_api_helper.dart
@@ -6,10 +6,10 @@ import '../models/finamp_models.dart';
 import '../models/jellyfin_models.dart';
 import 'finamp_settings_helper.dart';
 import 'finamp_user_helper.dart';
-import 'jellyfin_api.dart';
+import 'jellyfin_api.dart' as jellyfin_api;
 
 class JellyfinApiHelper {
-  final jellyfinApi = JellyfinApi.create();
+  final jellyfinApi = jellyfin_api.JellyfinApi.create();
   final _jellyfinApiHelperLogger = Logger("JellyfinApiHelper");
 
   // Stores the ids of the artists that the user selected to mix
@@ -19,6 +19,8 @@ class JellyfinApiHelper {
   List<BaseItemDto> selectedMixAlbums = [];
 
   Uri? baseUrlTemp;
+
+  final String defaultFields = jellyfin_api.defaultFields;
 
   final _finampUserHelper = GetIt.instance<FinampUserHelper>();
 
@@ -30,6 +32,7 @@ class JellyfinApiHelper {
     String? searchTerm,
     List<String>? itemIds,
     String? filters,
+    String? fields,
 
     /// The record index to start at. All items with a lower index will be
     /// dropped from the results.
@@ -63,6 +66,7 @@ class JellyfinApiHelper {
         parentId: parentItem.id,
         includeItemTypes: includeItemTypes,
         recursive: true,
+        fields: fields,
       );
     } else if (includeItemTypes == "MusicArtist") {
       // For artists, we need to use a different endpoint
@@ -76,6 +80,7 @@ class JellyfinApiHelper {
         startIndex: startIndex,
         limit: limit,
         userId: currentUser.id,
+        fields: fields,
       );
     } else if (parentItem?.type == "MusicArtist") {
       // For getting the children of artists, we need to use albumArtistIds
@@ -91,6 +96,7 @@ class JellyfinApiHelper {
         filters: filters,
         startIndex: startIndex,
         limit: limit,
+        fields: fields,
       );
     } else if (includeItemTypes == "MusicGenre") {
       response = await jellyfinApi.getGenres(
@@ -99,6 +105,7 @@ class JellyfinApiHelper {
         searchTerm: searchTerm,
         startIndex: startIndex,
         limit: limit,
+        fields: fields,
       );
     } else if (parentItem?.type == "MusicGenre") {
       response = await jellyfinApi.getItems(
@@ -112,6 +119,7 @@ class JellyfinApiHelper {
         filters: filters,
         startIndex: startIndex,
         limit: limit,
+        fields: fields,
       );
     } else {
       // This will be run when getting albums, songs in albums, and stuff like
@@ -128,6 +136,7 @@ class JellyfinApiHelper {
         startIndex: startIndex,
         limit: limit,
         ids: itemIds?.join(","),
+        fields: fields,
       );
     }
 

lib/components/AlbumScreen/downloaded_indicator.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
-import 'package:hive/hive.dart';
 
 import '../../models/finamp_models.dart';
-import '../../models/jellyfin_models.dart';
 import '../../services/isar_downloads.dart';
 import '../global_snackbar.dart';
 
@@ -21,7 +19,7 @@ class DownloadedIndicator extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isarDownloads = GetIt.instance<IsarDownloads>();
-    AsyncValue<DownloadItemState> status =
+    AsyncValue<DownloadItemState?> status =
         ref.watch(isarDownloads.stateProvider(item));
     if (status.hasValue) {
       switch (status.valueOrNull) {
@@ -30,7 +28,8 @@ class DownloadedIndicator extends ConsumerWidget {
           return const SizedBox.shrink();
         case DownloadItemState.enqueued:
         case DownloadItemState.downloading:
-          return Icon( //TODO invisible in light mode
+          return Icon(
+            //TODO invisible in light mode
             Icons.download_outlined,
             color: Colors.white.withOpacity(0.5),
             size: size,

lib/components/DownloadsScreen/downloaded_items_list.dart
@@ -8,8 +8,7 @@ import '../../services/isar_downloads.dart';
 import '../album_image.dart';
 import '../confirmation_prompt_dialog.dart';
 import '../first_page_progress_indicator.dart';
-import 'album_file_size.dart';
-import 'item_media_source_info.dart';
+import 'item_file_size.dart';
 
 class DownloadedItemsList extends StatefulWidget {
   const DownloadedItemsList({Key? key}) : super(key: key);
@@ -60,8 +59,8 @@ class _DownloadedItemsListState extends State<DownloadedItemsList> {
                         ),
                       ),
                     ),
-                    subtitle: AlbumFileSize(
-                      downloadedParent: album,
+                    subtitle: ItemFileSize(
+                      item: album,
                     ),
                     children: [
                       DownloadedChildrenList(
@@ -106,7 +105,7 @@ class _DownloadedChildrenListState extends State<DownloadedChildrenList> {
               ListTile(
                 title: Text(song.baseItem?.name ?? song.name),
                 leading: AlbumImage(item: song.baseItem),
-                subtitle: ItemMediaSourceInfo(
+                subtitle: ItemFileSize(
                   item: song,
                 ),
               )

lib/components/DownloadsScreen/item_file_size.dart
@@ -7,28 +7,42 @@ import 'package:get_it/get_it.dart';
 import '../../models/finamp_models.dart';
 import '../../services/isar_downloads.dart';
 
-class AlbumFileSize extends ConsumerWidget {
-  const AlbumFileSize({super.key, required this.downloadedParent});
+class ItemFileSize extends ConsumerWidget {
+  const ItemFileSize({super.key, required this.item});
 
-  final DownloadStub downloadedParent;
+  final DownloadStub item;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isarDownloader = GetIt.instance<IsarDownloads>();
     var downloadingText = AppLocalizations.of(context)!.activeDownloadSize;
-    var sizeText = ref
-        .watch(isarDownloader.stateProvider(downloadedParent).future)
-        .then((value1) {
+    var deletingText = AppLocalizations.of(context)!.missingDownloadSize;
+    Future<String> sizeText =
+        ref.watch(isarDownloader.stateProvider(item).future).then((value1) {
       switch (value1) {
+        case DownloadItemState.notDownloaded
+            when item.type == DownloadItemType.song:
+          return "0 MB Not Downloaded";
         case DownloadItemState.notDownloaded:
         case DownloadItemState.failed:
         case DownloadItemState.complete:
-          return isarDownloader
-              .getFileSize(downloadedParent)
-              .then((value2) => FileSize.getSize(value2));
+          if (item.type == DownloadItemType.song) {
+            var mediaSourceInfo = item.baseItem?.mediaSources?[0];
+            if (mediaSourceInfo == null) {
+              return "??? MB Unknown";
+            } else {
+              return "${FileSize.getSize(mediaSourceInfo.size)} ${mediaSourceInfo.container?.toUpperCase()}";
+            }
+          } else {
+            return isarDownloader
+                .getFileSize(item)
+                .then((value2) => FileSize.getSize(value2));
+          }
         case DownloadItemState.downloading:
         case DownloadItemState.enqueued:
           return Future.value(downloadingText);
+        case null:
+          return Future.value(deletingText);
       }
     });
     return FutureBuilder(

lib/components/DownloadsScreen/item_media_source_info.dart
@@ -1,38 +1,0 @@
-import 'package:file_sizes/file_sizes.dart';
-import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-
-import '../../models/finamp_models.dart';
-import '../../models/jellyfin_models.dart';
-import '../../services/isar_downloads.dart';
-
-class ItemMediaSourceInfo extends StatelessWidget {
-  const ItemMediaSourceInfo({super.key, required this.item});
-
-  final DownloadItem item;
-
-  @override
-  Widget build(BuildContext context) {
-    if (item.type != DownloadItemType.song) {
-      final isarDownloads = GetIt.instance<IsarDownloads>();
-      return FutureBuilder(
-          future: isarDownloads.getFileSize(item),
-          builder: (context, snapshot) {
-            if (snapshot.data == null) {
-              return const Text("??? MB Unknown");
-            } else {
-              return Text(FileSize.getSize(snapshot.data));
-            }
-          });
-    } else {
-      MediaSourceInfo? mediaSourceInfo = item.mediaSourceInfo;
-
-      if (mediaSourceInfo == null) {
-        return const Text("??? MB Unknown");
-      } else {
-        return Text(
-            "${FileSize.getSize(mediaSourceInfo.size)} ${mediaSourceInfo.container?.toUpperCase()}");
-      }
-    }
-  }
-}

lib/components/global_snackbar.dart
@@ -76,6 +76,9 @@ class GlobalSnackbar {
   static void error(dynamic event) => _enqueue(() => _error(event));
   static void _error(dynamic event) {
     _logger.warning("Displaying error: $event");
+    if (event is Error && event.stackTrace != null) {
+      _logger.warning(event.stackTrace);
+    }
     BuildContext context = materialAppNavigatorKey.currentContext!;
     String errorText;
     if (event is Response) {

lib/l10n/app_en.arb
@@ -819,6 +819,10 @@
   "@activeDownloadSize": {
     "description": "Message shown for download size while item is still being downloaded"
   },
+  "missingDownloadSize": "Deleting...",
+  "@missingDownloadSize": {
+    "description": "Message shown for download size while item is still being downloaded"
+  },
   "runRepairWarning": "The server could not be contacted to finalize downloads migration.  Please run 'Repair Downloads' from the downloads screen as soon as you are back online.",
   "@runRepairWarning": {
     "description": "Message shown when an error occurs during the downloads repair step of the Isar downloads migration, which requires a server connection."

lib/main.dart
@@ -71,11 +71,10 @@ void main() async {
     _setupOfflineListenLogHelper();
     await _setupDownloadsHelper();
     await _setupPlaybackServices();
-  } catch (e) {
+  } catch (error, trace) {
     hasFailed = true;
-    runApp(FinampErrorApp(
-      error: e,
-    ));
+    Logger("ErrorApp").severe(error, null, trace);
+    runApp(FinampErrorApp(error: error));
   }
 
   if (!hasFailed) {
@@ -397,7 +396,7 @@ class Finamp extends StatelessWidget {
 }
 
 class FinampErrorApp extends StatelessWidget {
-  const FinampErrorApp({Key? key, required this.error}) : super(key: key);
+  const FinampErrorApp({super.key, required this.error});
 
   final dynamic error;
 

commit ef47797c29aad050b8656db37001fe3f7e2405e2

fix defaultFields not being applied when fetching items
- fixes a regressing introduced in 9b983864b9f1c1a899fc4d7c6c3f6b351b862683

lib/services/jellyfin_api_helper.dart
@@ -119,6 +119,7 @@ class JellyfinApiHelper {
     }
     assert(!FinampSettingsHelper.finampSettings.isOffline);
     assert(itemIds == null || parentItem == null);
+    fields ??= defaultFields; // explicitly set the default fields, if we pass `null` to [JellyfinAPI.getItems] it will **not** apply the default fields, since the argument *is* provided.
     if (parentItem != null) {
       _jellyfinApiHelperLogger.fine("Getting children of ${parentItem.name}");
     } else if (itemIds != null) {
@@ -439,6 +440,7 @@ class JellyfinApiHelper {
   Future<BaseItemDto?> getItemByIdBatched(String itemId,
       [String? fields]) async {
     assert(!FinampSettingsHelper.finampSettings.isOffline);
+    fields ??= defaultFields; // explicitly set the default fields, if we pass `null` to [JellyfinAPI.getItems] it will **not** apply the default fields, since the argument *is* provided.
     _getItemByIdBatchedRequests.add(itemId);
     _getItemByIdBatchedFuture ??=
         Future.delayed(const Duration(milliseconds: 250), () async {