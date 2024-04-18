234f2843d4dbdd3fafcc0cb400069f65667867d6
Maxr1998 <max.rumpf1998@gmail.com>
2024-04-18 23:38:47 +0200

    Add missing apps/ subfolder to Linux icons path

--- assets/icon/generate_icons.sh
+++ assets/icon/generate_icons.sh
@@ -5,7 +5,7 @@ function generate_linux_icon() {
   inner_dim=$(($outer_dim * 3 / 4))
 
   foreground_temp="linux/icon_foreground_temp.png"
-  output_folder="linux/${outer_dim}x${outer_dim}"
+  output_folder="linux/${outer_dim}x${outer_dim}/apps"
 
   mkdir -p $output_folder
 
