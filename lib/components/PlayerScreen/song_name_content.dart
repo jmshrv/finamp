27708aaf81f9f51316f7b1a4558d488d6ba2c072
Anirudh Kurma <anirudhk313@gmail.com>
2024-05-28 08:30:58 -0400

    marquee in song menu for overflow, instead of two lines

--- lib/components/PlayerScreen/song_name_content.dart
+++ lib/components/PlayerScreen/song_name_content.dart
@@ -1,10 +1,10 @@
-import 'package:balanced_text/balanced_text.dart';
+import 'package:finamp/components/scrolling_text.dart';
+import 'package:flutter/material.dart';
 import 'package:finamp/components/AddToPlaylistScreen/add_to_playlist_button.dart';
 import 'package:finamp/components/PlayerScreen/player_buttons_more.dart';
 import 'package:finamp/models/finamp_models.dart';
 import 'package:finamp/models/jellyfin_models.dart' as jellyfin_models;
 import 'package:finamp/screens/player_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../services/queue_service.dart';
@@ -32,7 +32,6 @@ class SongNameContent extends StatelessWidget {
         }
 
         final currentTrack = snapshot.data!.currentTrack!;
-
         final jellyfin_models.BaseItemDto? songBaseItemDto =
             currentTrack.baseItem;
 
@@ -49,30 +48,58 @@ class SongNameContent extends StatelessWidget {
                   child: Container(
                     alignment: Alignment.center,
                     constraints: BoxConstraints(
-                      maxHeight:
-                          controller.shouldShow(PlayerHideable.twoLineTitle)
-                              ? 52
-                              : 24,
+                      maxHeight: 24,
                       maxWidth: 280,
                     ),
-                    child: BalancedText(
-                      currentTrack.item.title,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20,
-                        height: 26 / 20,
-                        fontWeight:
-                            Theme.of(context).brightness == Brightness.light
-                                ? FontWeight.w500
-                                : FontWeight.w600,
-                        overflow: TextOverflow.visible,
-                      ),
-                      softWrap: true,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines:
-                          controller.shouldShow(PlayerHideable.twoLineTitle)
-                              ? 2
-                              : 1,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        final textPainter = TextPainter(
+                          text: TextSpan(
+                            text: currentTrack.item.title,
+                            style: TextStyle(
+                              fontSize: 20,
+                              height: 26 / 20,
+                              fontWeight: Theme.of(context).brightness ==
+                                      Brightness.light
+                                  ? FontWeight.w500
+                                  : FontWeight.w600,
+                            ),
+                          ),
+                          maxLines: 1,
+                          textDirection: TextDirection.ltr,
+                        )..layout(maxWidth: constraints.maxWidth);
+
+                        final isOverflowing = textPainter.didExceedMaxLines;
+
+                        return Container(
+                          width: constraints.maxWidth,
+                          child: isOverflowing
+                              ? ScrollingText(
+                                  text: currentTrack.item.title,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    height: 26 / 20,
+                                    fontWeight: Theme.of(context).brightness ==
+                                            Brightness.light
+                                        ? FontWeight.w500
+                                        : FontWeight.w600,
+                                  ),
+                                )
+                              : Text(
+                                  currentTrack.item.title,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    height: 26 / 20,
+                                    fontWeight: Theme.of(context).brightness ==
+                                            Brightness.light
+                                        ? FontWeight.w500
+                                        : FontWeight.w600,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                ),
+                        );
+                      },
                     ),
                   ),
                 ),
---
+++ lib/services/scrolling_text_helper.dart
@@ -0,0 +1,46 @@
+import 'package:finamp/components/scrolling_text.dart';
+import 'package:flutter/material.dart';
+
+class ScrollingTextHelper extends StatelessWidget {
+  final String text;
+  final TextStyle? style;
+
+  const ScrollingTextHelper({
+    Key? key,
+    required this.text,
+    this.style,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final textPainter = TextPainter(
+          text: TextSpan(
+            text: text,
+            style: style,
+          ),
+          maxLines: 1,
+          textDirection: TextDirection.ltr,
+        )..layout(maxWidth: constraints.maxWidth);
+
+        final isOverflowing = textPainter.didExceedMaxLines;
+
+        return Container(
+          width: constraints.maxWidth,
+          child: isOverflowing
+              ? ScrollingText(
+                  text: text,
+                  style: style,
+                )
+              : Text(
+                  text,
+                  style: style,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+        );
+      },
+    );
+  }
+}
