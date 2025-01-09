6f2268d8c090a7681e64048bed52c7c8026f0497
aman-boop <58726697+aman-boop@users.noreply.github.com>
2025-01-09 22:51:17 +0530

    Update metadata_helper.dart

--- lib/services/metadata_helper.dart
+++ lib/services/metadata_helper.dart
@@ -144,7 +144,7 @@ class MetaData {
     await deviceInfo.init();
   }
 
-  Map<String, dynamic> toJson(MetaDataType type) {
+  Map<String, dynamic> toJson({required MetaDataType type}) {
     switch (type) {
       case MetaDataType.AppInfo:
         return appInfo.toJson();
