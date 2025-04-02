1948a5c3664671790c57950dc430f934029c7791
Lukas Lindner <git@lukaslindner.music>
2025-04-02 18:41:00 +0200

    Align ArtistTypeFilterChips left on wider screens

--- lib/components/MusicScreen/artist_type_selection_row.dart
+++ lib/components/MusicScreen/artist_type_selection_row.dart
@@ -7,59 +7,65 @@ import '../../services/finamp_settings_helper.dart';
 Widget buildArtistTypeSelectionRow(BuildContext context, TabContentType tabType, ArtistListType artistListType, Function(TabContentType) refreshTab) {
   if (tabType == TabContentType.artists) {
     var currentSettings = FinampSettingsHelper.finampSettings;
+    double screenWidth = MediaQuery.of(context).size.width;
+    bool alignLeft = screenWidth > 600;
 
     //Currently, album artists work only in online mode -> if we are in offline mode, we switch to performing artists and refresh
     if(currentSettings.isOffline && artistListType == ArtistListType.albumartist){
       FinampSettingsHelper.setArtistListType(ArtistListType.artist);
       refreshTab(tabType);
     }
-    return SizedBox(
-      height: 48,
-      width: double.infinity,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Opacity(
-            opacity: currentSettings.isOffline ? 0.4 : 1.0,
-            child: FilterChip(
-              label: Text(AppLocalizations.of(context)!.albumArtists),
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: SizedBox(
+        height: 48,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: alignLeft ? MainAxisAlignment.start : MainAxisAlignment.center,
+          children: [
+            Opacity(
+              opacity: currentSettings.isOffline ? 0.4 : 1.0,
+              child: FilterChip(
+                label: Text(AppLocalizations.of(context)!.albumArtists),
+                  onSelected: (_) {
+                  if (!currentSettings.isOffline) {
+                    FinampSettingsHelper.setArtistListType(ArtistListType.albumartist);
+                    refreshTab(tabType);
+                  }
+                },
+                selected: artistListType == ArtistListType.albumartist,
+                showCheckmark: false,
+                selectedColor: Theme.of(context).colorScheme.primary,
+                backgroundColor: Theme.of(context).colorScheme.surface,
+                labelStyle: TextStyle(
+                  color: artistListType == ArtistListType.albumartist
+                      ? Theme.of(context).colorScheme.onPrimary
+                      : Theme.of(context).colorScheme.onSurface,
+                ),
+                shape: StadiumBorder(),
+              ),
+            ),
+            SizedBox(width: 8),
+            FilterChip(
+              label: Text(AppLocalizations.of(context)!.performingArtists),
               onSelected: (_) {
-                if (!currentSettings.isOffline) {
-                  FinampSettingsHelper.setArtistListType(ArtistListType.albumartist);
-                  refreshTab(tabType);
-                }
-              },
-              selected: artistListType == ArtistListType.albumartist,
+                FinampSettingsHelper.setArtistListType(ArtistListType.artist);
+                refreshTab(tabType);
+              }, 
+              selected: artistListType == ArtistListType.artist,
               showCheckmark: false,
               selectedColor: Theme.of(context).colorScheme.primary,
               backgroundColor: Theme.of(context).colorScheme.surface,
               labelStyle: TextStyle(
-                color: artistListType == ArtistListType.albumartist
+                color: artistListType == ArtistListType.artist
                     ? Theme.of(context).colorScheme.onPrimary
-                    : Theme.of(context).colorScheme.onSurface,
+                    : Theme.of(context).colorScheme.onSurface
               ),
               shape: StadiumBorder(),
             ),
-          ),
-          SizedBox(width: 8),
-          FilterChip(
-            label: Text(AppLocalizations.of(context)!.performingArtists),
-            onSelected: (_) {
-              FinampSettingsHelper.setArtistListType(ArtistListType.artist);
-              refreshTab(tabType);
-            }, 
-            selected: artistListType == ArtistListType.artist,
-            showCheckmark: false,
-            selectedColor: Theme.of(context).colorScheme.primary,
-            backgroundColor: Theme.of(context).colorScheme.surface,
-            labelStyle: TextStyle(
-              color: artistListType == ArtistListType.artist
-                  ? Theme.of(context).colorScheme.onPrimary
-                  : Theme.of(context).colorScheme.onSurface
-            ),
-            shape: StadiumBorder(),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
