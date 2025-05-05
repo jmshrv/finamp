fe5aba055cb72b54a206e5b1c59277730d0708ff
Lukas Lindner <git@lukaslindner.music>
2025-05-06 00:26:44 +0200

    Add GenreChips to AlbumScreen.

--- lib/components/AlbumScreen/item_info.dart
+++ lib/components/AlbumScreen/item_info.dart
@@ -1,10 +1,12 @@
 import 'package:finamp/components/PlayerScreen/artist_chip.dart';
+import 'package:finamp/components/PlayerScreen/genre_chip.dart';
 import 'package:finamp/l10n/app_localizations.dart';
 import 'package:finamp/models/finamp_models.dart';
 import 'package:finamp/services/finamp_settings_helper.dart';
 import 'package:finamp/services/release_date_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 
 import '../../models/jellyfin_models.dart';
 import '../icon_and_text.dart';
@@ -23,6 +25,15 @@ class ItemInfo extends ConsumerWidget {
 // TODO: see if there's a way to expand this column to the row that it's in
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
+    final trackCountString = (itemTracks == (item.childCount ?? itemTracks) ||
+                      !ref.watch(finampSettingsProvider.isOffline))
+                  ? AppLocalizations.of(context)!.trackCount(itemTracks)
+                  : AppLocalizations.of(context)!
+                      .offlineTrackCount(item.childCount!, itemTracks);
+    final runtimeDuration = printDuration(item.runTimeTicksDuration());
+    final trackDurationString = "$trackCountString ($runtimeDuration)";
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -45,24 +56,41 @@ class ItemInfo extends ConsumerWidget {
         IconAndText(
             iconData: Icons.music_note,
             textSpan: TextSpan(
-              text: (itemTracks == (item.childCount ?? itemTracks) ||
-                      !ref.watch(finampSettingsProvider.isOffline))
-                  ? AppLocalizations.of(context)!.trackCount(itemTracks)
-                  : AppLocalizations.of(context)!
-                      .offlineTrackCount(item.childCount!, itemTracks),
+              text: trackDurationString,
             )),
+        /* Previously, we had the duration in one row
         IconAndText(
           iconData: Icons.timer,
           textSpan: TextSpan(
             text: printDuration(item.runTimeTicksDuration()),
           ),
-        ),
+        ),*/
         if (item.type != "Playlist")
           IconAndText(
               iconData: Icons.event,
               textSpan: TextSpan(
                 text: ReleaseDateHelper.autoFormat(item),
-              ))
+              )),
+        if (item.type != "Playlist" &&
+            (item.genreItems != null && item.genreItems!.isNotEmpty))
+          Row(
+            children: [
+              Icon(
+                TablerIcons.color_swatch,
+                color: theme.iconTheme.color?.withOpacity(
+                        theme.brightness == Brightness.light ? 0.38 : 0.5,
+                      ),
+              ),
+              const SizedBox(width: 4),
+              Expanded(
+                child: GenreChips(
+                        genres: item.genreItems!,
+                        backgroundColor:
+                          IconTheme.of(context).color!.withOpacity(0.1),
+                      ),
+              ),
+            ],
+          ),
       ],
     );
   }
