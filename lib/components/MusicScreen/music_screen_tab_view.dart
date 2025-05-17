3fe078d63ddfef2a0cbb887fb509b8a03cd57d1a
Lukas Lindner <git@lukaslindner.music>
2025-05-17 13:08:06 +0200

    Add tabOrder to MusicScreenContent refreshHash, so that tabs automatically reload once the tab order was changed.

--- lib/components/MusicScreen/music_screen_tab_view.dart
+++ lib/components/MusicScreen/music_screen_tab_view.dart
@@ -366,6 +366,7 @@ class _MusicScreenTabViewState extends State<MusicScreenTabView>
             widget.view?.id,
             settings.isOffline,
             settings.tabOrder.indexOf(widget.tabContentType),
+            settings.tabOrder,
             settings.trackOfflineFavorites,
             widget.genreFilter?.id,
           );
