6b1742c0b353da99f0bab8ae179bf8811abe1295
Maxr1998 <max.rumpf1998@gmail.com>
2025-06-08 19:09:17 +0200

    Rename my_application to finamp_application for Linux app

--- linux/main.cc
+++ linux/main.cc
@@ -1,6 +1,6 @@
-#include "my_application.h"
+#include "finamp_application.h"
 
 int main(int argc, char** argv) {
-  g_autoptr(MyApplication) app = my_application_new();
+  g_autoptr(FinampApplication) app = finamp_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
