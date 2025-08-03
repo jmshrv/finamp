1a8e6c45d3ca0a46cae6f0813d0b6b36e3ba526c
hammy275 <hammy275@gmail.com>
2025-08-03 15:01:35 -0400

    Add useRandomize to Settings Constructor

--- lib/models/finamp_models.dart
+++ lib/models/finamp_models.dart
@@ -356,6 +356,7 @@ class FinampSettings {
     this.rpcEnabled = DefaultSettings.rpcEnabled,
     this.rpcIcon = DefaultSettings.rpcIcon,
     this.preferAddingToFavoritesOverPlaylists = DefaultSettings.preferAddingToFavoritesOverPlaylists,
+    this.useRandomize = DefaultSettings.useRandomize,
   });
 
   @HiveField(0, defaultValue: DefaultSettings.isOffline)
@@ -751,6 +752,7 @@ class FinampSettings {
 
   @HiveField(126, defaultValue: DefaultSettings.preferAddingToFavoritesOverPlaylists)
   bool preferAddingToFavoritesOverPlaylists = DefaultSettings.preferAddingToFavoritesOverPlaylists;
+
   @HiveField(127, defaultValue: DefaultSettings.useRandomize)
   bool useRandomize = DefaultSettings.useRandomize;
 
