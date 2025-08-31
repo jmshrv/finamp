d295ce090c016c15a91aaef99049198950666d32
Lukas Lindner <git@lukaslindner.music>
2025-09-01 00:21:14 +0200

    Adjust Chip Colors.

--- lib/menus/components/playbackActions/playback_action_page_indicator.dart
+++ lib/menus/components/playbackActions/playback_action_page_indicator.dart
@@ -14,6 +14,19 @@ class PlaybackActionPageIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textColorSelected = compactLayout
+        ? TextStyle(fontSize: 13.0, color: Theme.of(context).colorScheme.onPrimary)
+        : TextStyle(fontSize: 13.0, color: Colors.white);
+    final textColor = compactLayout
+        ? TextStyle(fontSize: 13.0, color: Theme.of(context).colorScheme.onSurface)
+        : TextStyle(fontSize: 13.0, color: Colors.white);
+    final buttonColorSelected = compactLayout
+        ? Theme.of(context).colorScheme.primary
+        : Theme.of(context).colorScheme.primary.withOpacity(0.8);
+    final buttonColor = compactLayout
+        ? Theme.of(context).colorScheme.onSurface.withOpacity(0.1)
+        : Theme.of(context).colorScheme.onSurface.withOpacity(0.3);
+
     return Padding(
       padding: compactLayout ? EdgeInsets.only(top: 4.0) : EdgeInsets.only(bottom: 4.0),
       child: SingleChildScrollView(
@@ -38,13 +51,15 @@ class PlaybackActionPageIndicator extends StatelessWidget {
                     padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
                     decoration: BoxDecoration(
                       color: (pageController.page ?? pageController.initialPage).round() == index
-                          ? Theme.of(context).colorScheme.primary.withOpacity(0.8)
-                          : Theme.of(context).colorScheme.onSurface.withOpacity(0.3),
+                          ? buttonColorSelected
+                          : buttonColor,
                       borderRadius: BorderRadius.circular(9999.0),
                     ),
                     child: Text(
                       pages.keys.elementAt(index),
-                      style: const TextStyle(fontSize: 13.0, color: Colors.white),
+                      style: (pageController.page ?? pageController.initialPage).round() == index
+                          ? textColorSelected
+                          : textColor,
                     ),
                   ),
                 );
