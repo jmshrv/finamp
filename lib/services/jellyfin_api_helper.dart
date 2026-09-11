e94ab198856c00c33fa718b126c29a87c344f19f
martin-77 <52350479+martin-77@users.noreply.github.com>
2026-09-12 00:15:09 +0200

    Revert null-aware map element for generator compatibility

--- lib/services/jellyfin_api_helper.dart
+++ lib/services/jellyfin_api_helper.dart
@@ -1171,7 +1171,7 @@ class JellyfinApiHelper {
     List<String> builtPath = List<String>.from(parsedBaseUrl.pathSegments);
     builtPath.addAll(["Items", item.imageId!, "Images", "Primary"]);
     final Map<String, dynamic> queryParams = {
-      "format": ?format,
+      if (format != null) "format": format,
       if (quality != null) "quality": quality.toString(),
       if (maxWidth != null) "MaxWidth": maxWidth.toString(),
       if (maxHeight != null) "MaxHeight": maxHeight.toString(),
@@ -1208,7 +1208,7 @@ class JellyfinApiHelper {
       userInfo: baseUrl.userInfo,
       pathSegments: builtPath,
       queryParameters: {
-        "format": ?format,
+        if (format != null) "format": format,
         if (quality != null) "quality": quality.toString(),
         if (maxWidth != null) "MaxWidth": maxWidth.toString(),
         if (maxHeight != null) "MaxHeight": maxHeight.toString(),
